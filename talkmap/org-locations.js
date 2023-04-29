a6cf4e9c744f32a7a32e33e7b6511aa5e97cebdd
Selim Boukabara <106460801+selimboukabara@users.noreply.github.com>
2023-04-29 15:29:16 -0400

    Update org-locations.js
    Added place i been to

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,92 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
+    "College Park, MD",
+    38.9897, 
+    -76.9378
   ],
   [
-    "London, UK",
-    51.5073219,
-    -0.1276473
+    "Greenbelt, MD",
+    39.0046, 
+    -76.8755
   ],
   [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
+    "Baltimore, MD",
+    39.2904, 
+    -76.6122
   ],
   [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
+    "Chicago, IL",
+    41.0412, 
+    28.9977
+  ]
+  [
+    "New York City, NY",
+    40.7128, 
+    -74.0060
+  ]
+  [
+    "Meftah, Blida, Algeria"
+    36.6204, 
+    3.22248
+  ]
+  [
+    "Rome, Italy"
+    41.9028, 
+    12.4964
+  ]
+  [
+    "Paris, France"
+    48.8566, 
+    2.3522
+  ]
+  [
+    "Stanford, CA"
+    37.4275, 
+    -122.1697
+  ]
+  [
+    "Georgia"
+    32.1656, 
+    -82.9001
+  ]
+  [
+    "Florida"
+    27.6648, 
+    -81.5158
+  ]
+  [
+    "North Carolina"
+    35.7596, 
+    -79.0193
+  ]
+  [
+    "South Carolina"
+    33.8361, 
+    -81.1637
+  ]
+  [
+    "Virginia"
+    37.4316, 
+    -78.6569
+  ]
+  [
+    "West Virginia"
+    38.5976, 
+    -80.4549
+  ]
+  [
+    "Pennsylvania"
+    41.203, 
+    -77.1945
+  ]
+  [
+    "Arlington, MA"
+    38.8816, 
+    -77.0910
+  ]
+  [
+    "Quebec, Canada"
+    46.8131, 
+    -71.2075
   ]
 ];
